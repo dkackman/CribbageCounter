0d85908d28215534851c3111cc76a27a69add34b
Don Kackman <dkackman@gmail.com>
2019-12-01 19:29:33 -0600

    wire in explain

--- public/javascripts/rest.js
+++ public/javascripts/rest.js
@@ -1,8 +1,34 @@
-function getScore(elem)
+function getScore()
 {
-    $("#score").text("");
+    $("#score").html("");
     
     var url =  "api/score?hand=" + $("#handInputText").val();
+    url += "&isCrib=" + $("#isCribCheckBox").is(':checked');
+
+    $("#restArgs").text(document.baseURI + url);
+    $.ajax({
+        url: "/" + url,
+        type: "GET",
+        success: function(data) {
+            $("#score").text(data);            
+        },
+        error: function(data) {
+            console.log(data.status);
+            if (data.status == "400") {
+                $("#score").text(data.responseText);
+            }
+            else {
+                $("#score").text(data.statusText);
+            }
+        }
+    });
+}
+
+function explainScore()
+{
+    $("#score").html("");
+    
+    var url =  "api/explain?hand=" + $("#handInputText").val();
     url += "&isCrib=" + $("#isCribCheckBox").is(':checked');
 
     $("#restArgs").text(document.baseURI + url);
